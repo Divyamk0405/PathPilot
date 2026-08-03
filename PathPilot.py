6ab12db244a5bf49ece63826ae95a9828fd15b78
Divyamk0405 <76767549+Divyamk0405@users.noreply.github.com>
2026-08-03 22:46:01 +0530

    Add files via upload

--- PathPilot.py
+++ PathPilot.py
@@ -1,3 +1,14 @@
+import streamlit as st
+
+# Inject manifest and service worker
+st.markdown("""
+<link rel="manifest" href="manifest.json">
+<script>
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js');
+}
+</script>
+""", unsafe_allow_html=True)
 import random
 import streamlit as st
 
@@ -54,6 +65,18 @@ import random
 # Career Tab
 with tab2:
     st.subheader("Career Tab – Discover Rare Careers")
+    quiz_questions = [
+    "Do you enjoy solving puzzles and logical problems?",
+    "Would you like to explore space as an astronaut?",
+    "Do you prefer creating art or designing things?",
+    "Are you curious about how movies are made?",
+    "Do you enjoy helping people feel better?",
+    "Would you like to work with animals?",
+    "Do you enjoy experimenting with science?",
+    "Would you like to invent new technology?",
+    "Do you enjoy writing stories or scripts?",
+    "Would you like to explore the deep ocean?"
+]
 
     # Pool of questions
     questions_pool = [
